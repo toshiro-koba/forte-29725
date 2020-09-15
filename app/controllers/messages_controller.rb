42dd1389bb48cc23ba12d33f4023e0a74f97a810
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-16 08:21:30 +0900

    現在のユーザーに関係するかしないかを考慮するよう記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,29 @@
 class MessagesController < ApplicationController
   def index
     @rooms = Room.all.order('created_at DESC')
+    @entries = Entry.all.order('created_at DESC')
+    @questions = []
+    @question_ids = []
+    @another_questions = []
+    @rooms.each do |room|
+      @entries.each do |entry|
+        if entry.room_id == room.id
+          if entry.user_id == current_user.id
+              @questions << room
+              @question_ids << room.id
+          end
+        end
+      end
+    end
+
+    @another_question_ids = @rooms.ids - @question_ids
+
+    @rooms.each do |room|
+      @another_question_ids.each do |another_question_id|
+        if room.id == another_question_id
+          @another_questions << room
+        end
+      end
+    end
   end
 end
