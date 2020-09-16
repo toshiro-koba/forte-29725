a67b4405dca9e20c4f1331b3bbb6d9591887cddf
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-16 09:37:43 +0900

    roomモデルの単体テストコードを記述

---
+++ spec/models/room_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Room, type: :model do
+  describe '#create' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it "nameの値が存在すれば登録できること" do
+      expect(@room).to be_valid
+    end
+
+    it "nameが空では登録できないこと" do
+      @room.question_title = nil
+      @room.valid?
+      expect(@room.errors.full_messages).to include("Question title can't be blank")
+    end
+  end
+
+end
