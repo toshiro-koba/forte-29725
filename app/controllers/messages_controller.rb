def54cac5cb3ae2e0b1ad9d9467f8662ed64b7ee
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-16 12:12:31 +0900

    ルーとパスの指定を変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,29 +1,5 @@
 class MessagesController < ApplicationController
   def index
-    @rooms = Room.all.order('created_at DESC')
-    @another_questions = []
-    if user_signed_in?
-      @entries = Entry.all.order('created_at DESC')
-      @questions = []
-      @question_ids = []
-      @rooms.each do |room|
-        @entries.each do |entry|
-          next unless entry.room_id == room.id
-
-          if entry.user_id == current_user.id
-            @questions << room
-            @question_ids << room.id
-          end
-        end
-      end
-      @another_question_ids = @rooms.ids - @question_ids
-      @rooms.each do |room|
-        @another_question_ids.each do |another_question_id|
-          @another_questions << room if room.id == another_question_id
-        end
-      end
-    else
-      @another_questions = @rooms
-    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'messages#index'
-  resources :users, only: %i[edit update]
-  resources :rooms, only: %i[new create]
+  root to: 'rooms#index'
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:index, :new, :create] 
 end
