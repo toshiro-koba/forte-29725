7f035c4f7ebe538d9a0516b8158617a7eefd4312
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-16 16:02:38 +0900

    回答者以外は、回答ページのURLを直接入力しても、トップページに遷移する

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,22 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
+
+    @user_ids = []
+    @entries = Entry.all
+    @entries.each do |entry|
+      if entry.room_id == @room.id
+        @user_ids << entry.user_id
+      end
+    end
+    check = 0
+    @user_ids.each do |user|
+      if user == current_user.id
+        check += 1
+      end
+    end
+    binding.pry
+    redirect_to root_path unless check == 1 && @room.messages.size == 1  #現在のユーザーが質問に関連してかつ、未回答の場合のみ回答ページに飛ぶ
   end
 
   def create
@@ -22,3 +38,5 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
+
+
