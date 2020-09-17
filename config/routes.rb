f6e1e867e005c75afadccb0a8fb8f25e6477618f
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-17 13:54:36 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'rooms#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show] do
+    resources :gifts, only: [:index, :new, :create]
+  end
   resources :rooms, only: [:new, :create] do
     resources :messages, only: [:index, :create]
   end
