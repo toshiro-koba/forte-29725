e0628c275a4abeb9eceee0162c071716c5d27e01
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-17 14:43:03 +0900

    Revert "クレジットカード決済処理を設定"
    This reverts commit d1c6d0586ab1b0f56b254cbfbd5cabf23216eb01.

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -3,34 +3,4 @@ class GiftsController < ApplicationController
     @reciver = User.find(params[:user_id])
     @gift = Gift.new
   end
-
-  def new
-  end
-
-  def create
-    @reciver = User.find(params[:user_id])
-    @gift = Gift.new(order_params)
-    if @gift.valid?
-      pay_item
-      @gift.save
-      return redirect_to root_path
-    else
-      render 'index'
-    end
-  end
-
-  private
-
-  def order_params
-    params.require(:gift).permit(:price).merge(user_id: @reciver.id, giver_id: current_user.id)
-  end
-
-  def pay_item
-    Payjp.api_key = "sk_test_29f86d855ba21782a470757f"  # PAY.JPテスト秘密鍵
-    Payjp::Charge.create(
-      amount: order_params[:price], # 商品の値段
-      card: params[:token], # カードトークン
-      currency: 'jpy'                 # 通貨の種類(日本円)
-    )
-  end
 end
--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,8 +1,4 @@
 class Gift < ApplicationRecord
   belongs_to :user
   belongs_to :giver, class_name: 'User'
-
-  validates :user_id, presence: true
-  validates :giver_id, presence: true
-  validates :price, presence: true
 end
