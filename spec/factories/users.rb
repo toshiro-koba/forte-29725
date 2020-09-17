63546fbcf12fbac6574e44f991ae719010b629e9
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-17 18:18:29 +0900

    giftモデルの単体テストコードを途中まで記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     password {password}
     password_confirmation {password}
   end
+
+  factory :giver, class: User do
+    nickname {Faker::Name.first_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 8)
+    password {password}
+    password_confirmation {password}
+  end
 end
