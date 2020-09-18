933d9dfeb450a36c2cb81af4b604bf0cead78e26
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-18 09:18:51 +0900

    searchアクションを設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -40,6 +40,34 @@ class RoomsController < ApplicationController
     end
   end
 
+  def search
+    @rooms = Room.search(params[:keyword])
+
+    @another_questions = []
+    if user_signed_in?
+      @entries = Entry.all.order('created_at DESC')
+      @questions = []
+      @question_ids = []
+      @rooms.each do |room|
+        @entries.each do |entry|
+          next unless entry.room_id == room.id
+          if entry.user_id == current_user.id
+            @questions << room
+            @question_ids << room.id
+          end
+        end
+      end
+      @another_question_ids = @rooms.ids - @question_ids
+      @rooms.each do |room|
+        @another_question_ids.each do |another_question_id|
+          @another_questions << room if room.id == another_question_id
+        end
+      end
+    else
+      @another_questions = @rooms
+    end
+  end
+
   private
 
   def room_params
