04b6daa2b411dc1540752508a2e0563768b919b3
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-19 17:17:38 +0900

    bookmarksテーブルの作成

---
+++ db/migrate/20200919080942_create_bookmarks.rb
@@ -0,0 +1,9 @@
+class CreateBookmarks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookmarks do |t|
+      t.references :user, foreign_key: true
+      t.references :game_tag, foreign_key: true
+      t.timestamps
+    end
+  end
+end
