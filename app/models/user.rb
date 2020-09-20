11c98fd943903691884750d3de55f2b9338b0005
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-20 18:08:40 +0900

    アソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,8 @@ class User < ApplicationRecord
   has_many :receivings, through: :reverse_of_gifts, source: :user
   has_many :bookmarks
   has_many :game_tags, through: :bookmarks
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 end
