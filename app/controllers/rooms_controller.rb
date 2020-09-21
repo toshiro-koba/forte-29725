ace3e88734094a2c966657a463663f1b6ebb0b3d
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-21 15:38:09 +0900

    roomsコントローラーのsearchアクションをリファクタリング

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -29,26 +29,14 @@ class RoomsController < ApplicationController
 
   def search
     @rooms = Room.search(params[:keyword])
-    @another_questions = []
     if user_signed_in?
-      @entries = Entry.all.order('created_at DESC')
-      @questions = []
-      @question_ids = []
+      @questions = []  
       @rooms.each do |room|
-        @entries.each do |entry|
-          next unless entry.room_id == room.id
-          if entry.user_id == current_user.id
-            @questions << room
-            @question_ids << room.id
-          end
+        if  room.user_ids.include?(current_user.id)
+          @questions << room
         end
       end
-      @another_question_ids = @rooms.ids - @question_ids
-      @rooms.each do |room|
-        @another_question_ids.each do |another_question_id|
-          @another_questions << room if room.id == another_question_id
-        end
-      end
+      @another_questions = @rooms - @questions
     else
       @another_questions = @rooms
     end
