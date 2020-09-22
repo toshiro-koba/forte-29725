1eac67e5979b9adb4c63114cdea4fc51c0f98fff
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-22 18:27:13 +0900

    profileモデルの単体コード

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @profile = FactoryBot.build(:profile)
+    end
+
+    it 'userが紐づいていれば保存できること' do
+      expect(@profile).to be_valid
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @profile.user = nil
+      @profile.valid?
+      expect(@profile.errors.full_messages).to include('User must exist')
+    end
+  end
 end
