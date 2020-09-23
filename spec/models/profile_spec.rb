881fc1798597ff7f1bdebaa28124fcc6188240c7
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-23 14:39:09 +0900

    profileモデルの単体テストコードにimageカラムを追加

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
+  include CarrierWave::Test::Matchers
   describe '#create' do
     before do
       @profile = FactoryBot.build(:profile)
@@ -10,10 +11,25 @@ RSpec.describe Profile, type: :model do
       expect(@profile).to be_valid
     end
 
+    it "imageの形式がjpg, jpeg, gif, pngであれば、保存できる" do
+      formats = %w(jpg jpeg gif png)
+      formats.each do |format|
+       image_path = File.join(Rails.root, "spec/fixtures/sample.#{format}")
+       @profile = FactoryBot.build(:profile, image: File.open(image_path))
+       expect(@profile).to be_valid
+      end
+     end
+
     it 'userが紐付いていないと保存できないこと' do
       @profile.user = nil
       @profile.valid?
       expect(@profile.errors.full_messages).to include('User must exist')
     end
   end
+
+   it "imageの形式がrbだと、保存できない" do
+    image_path = File.join(Rails.root, "spec/fixtures/sample.rb")
+    @profile = FactoryBot.build(:profile, image: File.open(image_path))
+    expect(user).not_to be_valid
+   end
 end
