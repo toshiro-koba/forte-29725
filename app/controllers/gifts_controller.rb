2f25e858bb0bb55a86b116bd8f50b0b6a7848e72
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-03 16:36:54 +0900

    ギフティングに必要な各要素を配置

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -2,6 +2,7 @@ class GiftsController < ApplicationController
   def index
     redirect_to new_user_session_path unless user_signed_in?
     @reciver = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     redirect_to root_path if @reciver == current_user
     @gift = Gift.new
   end
