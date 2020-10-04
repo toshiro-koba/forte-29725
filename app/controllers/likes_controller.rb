a2f763eb52db1c5ae24369680cfc571fd86412ea
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-04 10:12:09 +0900

    コントローラ側の処理の設定

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+  before_action :set_like
+
+  def create
+      user = current_user
+      room = Room.find(params[:room_id])
+      like = Like.create(user_id: user.id, room_id: room.id)
+  end
+  def destroy
+      user = current_user
+      room = Room.find(params[:room_id])
+      like = Like.find_by(user_id: user.id, room_id: room.id)
+      like.delete
+  end
+
+  private
+  def set_like
+      @room = Room.find(params[:room_id])
+  end
+end
