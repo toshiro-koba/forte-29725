3be0b3e0bf76d4c02d60aaf1fd036579330953de
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-11 17:18:05 +0900

    [testsessionsコントローラーの単体テスト]テストユーザーログインが成功することを確認するテストを記述しました

--- spec/requests/testsessions_request_spec.rb
+++ spec/requests/testsessions_request_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Testsessions', type: :request do
+  before do
+    @user = FactoryBot.create(:user, email: 'test_user@test.com')
+  end
+
+  describe "update" do
+    example "トップページへリダイレクトされること" do
+      post testsessions_path
+      expect(response).to redirect_to root_path
+    end
+  end
 end
