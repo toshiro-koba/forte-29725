48defaaeba31d3d55441b4d23a2d5fb04a8a1af1
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-11 14:40:29 +0900

    [ギフト金額にバリデーションを設定]300~9,999,999円の範囲内ならギフトできるように変更しました

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -8,4 +8,8 @@ class Gift < ApplicationRecord
   validates :giver_id, presence: true
   validates :price, presence: true
   validates :token, presence: true
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, message: 'Out of setting range', less_than_or_equal_to: 9_999_999 }
+
+  validates :price, numericality: { only_integer: true, message: 'Half-width number' }
 end
