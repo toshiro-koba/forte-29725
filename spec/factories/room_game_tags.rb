4099d111087f0336cf9c466f6a5eb28c54828346
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-12 11:45:13 +0900

    [room_game_tagモデルの単体テスト]ゲームタグお気に入り機能のテストを記述しました

--- spec/factories/room_game_tags.rb
+++ spec/factories/room_game_tags.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :room_game_tag do
+    association :room
+    association :game_tag
   end
 end
--- spec/models/room_game_tag_spec.rb
+++ spec/models/room_game_tag_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe RoomGameTag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @room_game_tag = FactoryBot.build(:room_game_tag)
+    end
+
+    it 'roomとgame_tagが紐づいていれば保存できること' do
+      expect(@room_game_tag).to be_valid
+    end
+
+    it 'roomが紐付いていないと保存できないこと' do
+      @room_game_tag.room = nil
+      @room_game_tag.valid?
+      expect(@room_game_tag.errors.full_messages).to include('Room must exist')
+    end
+
+    it 'game_tagが紐付いていないと保存できないこと' do
+      @room_game_tag.game_tag = nil
+      @room_game_tag.valid?
+      expect(@room_game_tag.errors.full_messages).to include('Game tag must exist')
+    end
+  end
 end
