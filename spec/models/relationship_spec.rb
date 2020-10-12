c63fbafb96161b5d7a29761e7e11cab0af109b9b
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-12 11:39:21 +0900

    [relationモデルの単体テスト]フォロー機能のテストを記述しました

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:follow_user) { FactoryBot.create(:user) }
+  let(:active) { user.relationships.build(follow_id: follow_user.id) }
+  subject { active }
+
+  # フォローが有効であること
+  it { should be_valid }
+
+  describe "create"
+    it "follow_userがいれば、フォローができること" do
+      expect(active.follow). to eq follow_user
+    end
+
+    it "followが紐付いていないとフォローできないこと" do
+      active.follow = nil
+      active.valid?
+      expect(active.errors.full_messages).to include("Follow must exist")
+    end
 end
