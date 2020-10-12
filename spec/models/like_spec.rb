c27b056853b2f3af8ea96957c346a79930344998
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-12 11:38:48 +0900

    [likeモデルの単体テスト]いいね機能のテストを記述しました

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @like = FactoryBot.build(:like)
+    end
+
+
+    it 'roomが紐付いていないと保存できないこと' do
+      @like.room = nil
+      @like.valid?
+      expect(@like.errors.full_messages).to include('Room must exist')
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @like.user = nil
+      @like.valid?
+      expect(@like.errors.full_messages).to include('User must exist')
+    end
+  end
 end
