7feb59e019d4da4976c857127ccb5087a6a10d8f
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-14 16:50:38 +0900

    [通知機能の実装]「質問回答」に関する通知アクションを作成しました

--- app/models/room.rb
+++ app/models/room.rb
@@ -40,4 +40,29 @@ class Room < ApplicationRecord
       notification.save if notification.valid?
     end
   end
+
+  def create_notification_comment!(current_user, message_id)
+    # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
+    temp_ids = Message.select(:user_id).where(room_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, message_id, temp_id['user_id'])
+    end
+    # まだ誰もコメントしていない場合は、投稿者に通知を送る
+    save_notification_comment!(current_user, message_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_user, message_id, visited_id)
+    # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
+    notification = current_user.active_notifications.new(
+      room_id: id,
+      message_id: message_id,
+      visited_id: visited_id,
+      action: 'message'
+    )
+    # 自分の投稿に対するコメントの場合は、通知済みとする
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
 end
