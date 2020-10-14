0fc458626d29a932af037b85d398653937946362
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-14 22:32:09 +0900

    [通知機能の実装]質問者と回答者への「いいね」通知レコードのコミットの設定を、変更しました

--- app/models/room.rb
+++ app/models/room.rb
@@ -25,19 +25,30 @@ class Room < ApplicationRecord
 
   def create_notification_like!(current_user)
     # すでに「いいね」されているか検索
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and room_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and room_id = ? and action = ? ", current_user.id, users[1].id, id, 'like'])
     # いいねされていない場合のみ、通知レコードを作成
     if temp.blank?
-      notification = current_user.active_notifications.new(
+      # 回答者への通知インスタンスを作成
+      notification_answer = current_user.active_notifications.new(
         room_id: id,
-        visited_id: user_id,
+        visited_id: users[1].id,
+        action: 'like'
+      )
+      # 質問者への通知インスタンスを作成
+      notification_questioner = current_user.active_notifications.new(
+        room_id: id,
+        visited_id: users[0].id,
         action: 'like'
       )
       # 自分自身の投稿に対するいいねの場合は、通知済みとする
-      if notification.visitor_id == notification.visited_id
-        notification.checked = true
+      if notification_answer.visitor_id == current_user.id || notification_answer.visited_id == current_user.id
+        notification_answer.checked = true
       end
-      notification.save if notification.valid?
+      # 質問者と回答者への通知をレコードにコミットする
+      if notification_answer.valid?
+        notification_answer.save
+        notification_questioner.save
+      end
     end
   end
 
