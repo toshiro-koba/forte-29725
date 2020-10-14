e3a326f14e99ab9117089ec49d941b95954569b4
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-14 16:45:44 +0900

    [通知機能の実装]「いいね」に関する通知アクションを作成しました

--- app/models/room.rb
+++ app/models/room.rb
@@ -22,4 +22,22 @@ class Room < ApplicationRecord
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
+
+  def create_notification_like!(current_user)
+    # すでに「いいね」されているか検索
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and room_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        room_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+      # 自分自身の投稿に対するいいねの場合は、通知済みとする
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
 end
