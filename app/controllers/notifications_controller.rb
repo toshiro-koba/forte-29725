3c49bbf68f03d7223392388bbf0f4811cc63713d
toshiro-koba <kobato.231.0625@gmail.com>
2020-10-16 17:11:40 +0900

    [通知一覧ページ]全体のレイアウトを改善しました

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,6 @@
 class NotificationsController < ApplicationController
   def index
+    @user = current_user
     @notifications = current_user.passive_notifications
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
