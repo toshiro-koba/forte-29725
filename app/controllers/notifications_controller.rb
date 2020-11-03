056f0c3416834bbde7eeffa3275e9d94a927ce95
toshiro-koba <kobato.231.0625@gmail.com>
2020-11-03 12:09:54 +0900

    [通知一覧表示]自分のアクションが通知一覧に表示される問題を、修正しました

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,5 +5,6 @@ class NotificationsController < ApplicationController
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
+    @notifications = @notifications.where.not(visitor_id: current_user.id)
   end
 end
