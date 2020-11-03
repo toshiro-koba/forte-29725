38211fdc5bc4abdf8eb50edfb04d8c0865cb5504
toshiro-koba <kobato.231.0625@gmail.com>
2020-11-03 12:17:23 +0900

    [リファクタリング]Notificationsコントローラーindexアクションの記述を、モデルに切り出しました

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,4 +5,13 @@ class Notification < ApplicationRecord
 
   belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+
+  def self.check(user)
+    notifications = user.passive_notifications
+    notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+    notifications = notifications.where.not(visitor_id: user.id)
+    return notifications
+  end
 end
