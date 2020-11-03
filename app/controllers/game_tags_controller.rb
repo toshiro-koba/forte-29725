f0c34fb9f10f275939895145226d712ab34499a0
toshiro-koba <kobato.231.0625@gmail.com>
2020-11-04 08:34:40 +0900

    [ゲームタグ検索機能]tag_searchアクションの処理を、記述しました

--- app/controllers/game_tags_controller.rb
+++ app/controllers/game_tags_controller.rb
@@ -1,5 +1,12 @@
 class GameTagsController < ApplicationController
   def tag_search
-    
+    game_tag = params[:id].to_i
+    rooms = Room.preload(:likes, :game_tags, messages: :user).all.order('created_at DESC')
+    @questions = []
+    rooms.each do |room|
+      unless room.game_tags[0] == nil
+        @questions << room if room.game_tags[0].id == game_tag
+      end
+    end
   end
 end
