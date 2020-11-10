9455f111a34718280c820ed6de99b5c9e28f4406
toshiro-koba <56726628+toshiro-koba@users.noreply.github.com>
2020-11-10 14:19:59 +0900

    Revert "テストコードの追加"

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,10 +1,4 @@
 FactoryBot.define do
   factory :notification do
-    visitor_id { Faker::Number.between(from: 1, to: 5) }
-    visited_id { Faker::Number.between(from: 6, to: 10) }
-    room_id { Faker::Number.between(from: 1, to: 5) }
-    message_id { Faker::Number.between(from: 1, to: 5) }
-    action { 'message' }
-    checked { 0 }
   end
 end
