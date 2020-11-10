c04e157d23cc5e17dfa5ae6117249d1ad105a0b5
toshiro-koba <kobato.231.0625@gmail.com>
2020-11-10 14:15:59 +0900

    [テストの修正]サインインフォームを一つ入力するたびに、待機時間を設定しました

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -2,7 +2,9 @@ module SignInSupport
   def sign_in(user)
     visit root_path
     click_on('ログイン')
+    sleep 0.1
     fill_in 'メールアドレス', with: user.email
+    sleep 0.1
     fill_in 'パスワード', with: user.password
     find('.login__hover').hover
     find('input[name="commit"]').click
