b7225f4daed52a459648b70fde69d10fe36631ba
toshiro-koba <kobato.231.0625@gmail.com>
2020-11-12 17:16:19 +0900

    [game_tagsコントローラー]ログアウト状態で直接URLを入力したら、ログイン画面へ遷移するように設定しました

--- app/controllers/game_tags_controller.rb
+++ app/controllers/game_tags_controller.rb
@@ -1,4 +1,6 @@
 class GameTagsController < ApplicationController
+  before_action :check_if_user_signed_in
+
   def tag_search
     game_tag = params[:id].to_i
     rooms = Room.preload(:likes, :game_tags, messages: :user).all.order('created_at DESC')
@@ -8,5 +10,11 @@ class GameTagsController < ApplicationController
         @questions << room if room.game_tags[0].id == game_tag
       end
     end
+  end
+
+  private
+
+  def check_if_user_signed_in
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
