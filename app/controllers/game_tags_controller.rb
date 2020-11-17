76bb7e71892dd5747967b344c856867ce6507f58
toshiro-koba <kobato.231.0625@gmail.com>
2020-11-17 21:17:30 +0900

    [リファクタリング]ゲームタグ検索に関する記述をリファクタリングしました

--- app/controllers/game_tags_controller.rb
+++ app/controllers/game_tags_controller.rb
@@ -6,7 +6,7 @@ class GameTagsController < ApplicationController
     rooms = Room.preload(:likes, :game_tags, messages: :user).all.order('created_at DESC')
     @questions = []
     rooms.each do |room|
-      unless room.game_tags[0] == nil
+      if room.game_tags[0]
         @questions << room if room.game_tags[0].id == game_tag
       end
     end
